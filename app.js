5b038bafe8b1a16980bfcf57f4e095cf9fc30574
shiv dubey <dubeyshivprasad98@gmail.com>
2020-07-05 20:41:10 +0530

    basic routes and views created

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-var catalogRouter = require("./routes/catalog");
 require("dotenv").config();
 
 var app = express();
@@ -30,7 +29,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
